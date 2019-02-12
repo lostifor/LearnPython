8e170dd453cf88248ca850855d56dce9d77c6a56
apple <apple@appledeiMac.local>
2019-02-12 16:11:33 +0800

    base

---
+++ pysty/PythonBase/do_if.py
@@ -0,0 +1,7 @@
+x = int(input('请输入第一个数字x:')) 
+y = int(input('请输入第二个数字y:'))
+
+if x >= y:
+    print('第一个数字大：',x)
+else:
+    print('第二个数字大：',y)
---
+++ pysty/PythonBase/do_str.py
@@ -0,0 +1,14 @@
+# 'I\'m \"OK\"!'
+print('I\'mOk.')
+
+# 用r''表示''内部的字符串默认不转义
+# print('\\\t\\\')
+
+# print(r'\\\t\\\')
+
+print(''''这是一段多行字符串。这是它的第一行。
+This is the second line.
+"What's your name?," I asked.
+He said "Bond, James Bond."
+''')
+
