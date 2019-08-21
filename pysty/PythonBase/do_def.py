9f6ea77645beabd4af6758cef97b6ff80a7510f4
Lostifor <>
2019-08-21 16:05:30 +0800

    dd

--- pysty/PythonBase/do_def.py
+++ pysty/PythonBase/do_def.py
@@ -21,9 +21,69 @@ def printme( str ):
 printme("调用printme函数！")
 printme("再次调用")
 
+
+
 def area(width,height):
     return width*height
 
 w = 4
 h = 5
 print("area = ",area(w,h))
+
+# 参数传递
+# 在 python 中，strings, tuples, 和 numbers 是不可更改的对象，而 list,dict 等则是可以修改的对象。
+#   1.不可变类型：变量赋值 a=5 后再赋值 a=10，这里实际是新生成一个 int 值对象 10，再让 a 指向它，而 5 被丢弃，不是改变a的值，相当于新生成了a。
+#   2.可变类型：变量赋值 la=[1,2,3,4] 后再赋值 la[2]=5 则是将 list la 的第三个元素值更改，本身la没有动，只是其内部的一部分值被修改了。
+
+# python 函数的参数传递：
+# 不可变类型：类似 c++ 的值传递，如 整数、字符串、元组。如fun（a），传递的只是a的值，没有影响a对象本身。比如在 fun（a）内部修改 a 的值，只是修改另一个复制的对象，不会影响 a 本身。
+# 可变类型：类似 c++ 的引用传递，如 列表，字典。如 fun（la），则是将 la 真正的传过去，修改后fun外部的la也会受影响
+
+# 默认参数
+# 调用函数时，如果没有传递参数，则会使用默认参数。以下实例中如果没有传入 age 参数，则使用默认值：
+#可写函数说明
+def printinfo( name, age = 35 ):
+   "打印任何传入的字符串"
+   print ("名字: ", name)
+   print ("年龄: ", age)
+   return
+ 
+#调用printinfo函数
+printinfo( age=50, name="runoob" )
+print ("------------------------")
+printinfo( name="runoob" )
+
+# 不定长参数  加了星号 * 的参数会以元组(tuple)的形式导入，存放所有未命名的变量参数。
+def printinfo( arg1, *vartuple ):
+   "打印任何传入的参数"
+   print ("输出: ")
+   print (arg1)
+   print (vartuple)
+# 调用printinfo 函数
+printinfo( 70, 60, 50 )
+# 输出: 
+# 70
+# (60, 50)
+
+# 如果在函数调用时没有指定参数，它就是一个空元组。我们也可以不向函数传递未命名的变量
+def printinfo( arg1, *vartuple ):
+   "打印任何传入的参数"
+   print ("输出: ")
+   print (arg1)
+   for var in vartuple:
+      print (var)
+   return
+ 
+# 调用printinfo 函数
+printinfo( 10 )
+printinfo( 70, 60, 50 )
+
+# 加了两个星号 ** 的参数会以字典的形式导入。
+def printinfo( arg1, **vardict ):
+   "打印任何传入的参数"
+   print ("输出: ")
+   print (arg1)
+   print (vardict)
+ 
+# 调用printinfo 函数
+printinfo(1, a=2,b=3)
